84e0bfa6b6ddb0657c23e00acb2c3d53b58e3d7d
Martin Pamukchiev <martin.t.pamukchiev@gmail.com>
2026-05-14 10:02:13 +0300

    Initial PWA: taxi exam app with manifest, service worker, and icons

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'taxi-exam-v1';
+const ASSETS = [
+  '/taxi-exam/',
+  '/taxi-exam/index.html',
+  '/taxi-exam/manifest.json',
+  '/taxi-exam/icons/icon-192.png',
+  '/taxi-exam/icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
